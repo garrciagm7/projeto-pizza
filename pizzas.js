92a994852bacdfa27b9f2d26d08f717b71c97445
Gabriel Garcia <gabriel.garrcia23@outlook.com>
2024-09-04 17:28:35 -0300

    Initial commit

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Uma deliciosa combinação de Linguiça Calabresa, rodelas de cebolas frescas, azeitonas pretas, mussarela, polpa de tomate, orégano e massa especial'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Quatro queijos é coberta com os deliciosos queijos gorgonzola, parmesão, provolone e mussarela. A massa é fermentada de forma tradicional'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Tomate, Muçarela, Creme de ricota, Lombo canadense, Frango, Milho, Orégano, Passas, Presunto, Ovos'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Queijo mussarela e sorvete'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'linguiça calabresa, cebola e pimenta dedo-de-moça'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Chocolate meio amargo, queijo mussarela e m&ms'}
+];
